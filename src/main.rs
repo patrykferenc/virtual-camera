347b00b9e6af30820bd4e444cd0776490fbae43b
Patryk Ferenc <patrykferenc@protonmail.com>
2023-04-17 22:23:09 +0200

    SDL-2 Setup.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
-fn main() {
-    println!("Hello, world!");
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+
+pub fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let window = video_subsystem
+        .window("Virtual camera with rust-sdl2 demo: Window", 800, 600)
+        .resizable()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut canvas = window
+        .into_canvas()
+        .present_vsync()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut tick = 0;
+
+    let mut event_pump = sdl_context.event_pump().map_err(|e| e.to_string())?;
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+
+        {
+            // Update the window title.
+            let window = canvas.window_mut();
+
+            let position = window.position();
+            let size = window.size();
+            let title = format!(
+                "Window - pos({}x{}), size({}x{}): {}",
+                position.0, position.1, size.0, size.1, tick
+            );
+            window.set_title(&title).map_err(|e| e.to_string())?;
+
+            tick += 1;
+        }
+
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
+        canvas.set_draw_color(Color::RGB(0, 255, 255));
+        canvas.draw_point((100, 100)).map_err(|e| e.to_string())?;
+        canvas.present();
+    }
+
+    Ok(())
 }
